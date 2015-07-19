be37a5d33c338a09ad3e79620c9f0a90e85040dc
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-19 00:35:02 -0400

    Update and rename README.md to BasicCalculator.java

---
+++ BasicCalculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class apples{
+	public static void main(String args[]){
+	Scanner UserTyped = new Scanner(System.in);
+	
+	double fnum, snum, answer;
+	System.out.println("Enter a number: ");
+	fnum = UserTyped.nextDouble();
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Program-6
-Building a Basic Calculator
